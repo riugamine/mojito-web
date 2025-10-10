5357e89147201a8b937860b154c1596db25878a2
riugamine <jangel9829@gmail.com>
2025-10-10 16:26:38 -0400

    Update favicon in index.html and integrate Navbar component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 gsap.registerPlugin(ScrollTrigger, SplitText);
-
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className='flex-center h-[100vh]'>
-        <h1 className='text-4xl font-bold'>Hello World</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   )
 } 
 
