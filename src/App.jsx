81a440547cc50636bca35da064750ad484b47348
riugamine <jangel9829@gmail.com>
2026-02-24 15:23:01 -0400

    feat: implement contact section with a new component, centralize application constants, and integrate into the main application.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Cocktails from './components/Cocktails'
 import About from './components/About'
 import Art from './components/Art'
 import Menu from './components/Menu'
+import Contact from './components/Contact'
 const App = () => {
   return (
     <main>
@@ -17,6 +18,7 @@ const App = () => {
       <About />
       <Art />
       <Menu />
+      <Contact />
     </main>
   )
 } 
